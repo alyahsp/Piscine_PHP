624600a0c838fe64e08508927bbd32b0a7fcec67
Stanya Palmaro <spalmaro@41.student.fr>
2017-03-30 13:53:53 +0200

    same probs

--- j01/ex11/do_op_2.php
+++ j01/ex11/do_op_2.php
@@ -1,4 +1,31 @@
 #!/usr/bin/php
 <?php
-
+if ($argc == 2)
+{
+	$equ = trim($argv[1]);
+	$equ = preg_replace('/[*]/', " * ", $equ);
+	$equ = preg_replace('/[-]/', " - ", $equ);
+	$equ = preg_replace('/[+]/', " + ", $equ);
+	$equ = preg_replace('/[\/]/', " / ", $equ);
+	$equ = preg_replace('/[%]/', " % ", $equ);
+	$equ = preg_replace('/\s+/', ' ', $equ);
+	$equ = explode(" ", $equ);
+	if (!is_numeric($equ[0]) || !is_numeric($equ[2]) || count($equ) != 3)
+	{
+		echo "Syntax Error\n";
+		exit();
+	}
+	if (!strcmp($equ[1], "+"))
+		echo ($equ[0] + $equ[2]) . "\n";
+	else if (!strcmp($equ[1], "-"))
+		echo ($equ[0] - $equ[2]) . "\n";
+	else if (!strcmp($equ[1], "*"))
+		echo ($equ[0] * $equ[2]) . "\n";
+	else if (!strcmp($equ[1], "/"))
+		echo ($equ[0] / $equ[2]) . "\n";
+	else if (!strcmp($equ[1], "%"))
+		echo ($equ[0] % $equ[2]) . "\n";
+}
+else
+	echo "Incorrect Parameters\n";
 ?>
---
+++ j01/ex12/search_it!.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+if ($argc > 2)
+{
+	$key = $argv[1];
+	
+}
+else {
+	return ;
+}
+?>
