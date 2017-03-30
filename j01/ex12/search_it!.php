9486cf9480bf6ec901e031974252fc85678c5919
Stanya PALMARO <spalmaro@e2r5p19.42.fr>
2017-03-30 16:21:54 +0200

    ex12 works not ex09

--- j01/ex12/search_it!.php
+++ j01/ex12/search_it!.php
@@ -2,8 +2,24 @@
 <?php
 if ($argc > 2)
 {
+	unset($argv[0]);
 	$key = $argv[1];
-	
+	unset($argv[1]);
+	$str = implode(":", $argv);
+	$str = explode(":", $str);
+	$i = 0;
+	$hel = false;
+	while ($str[$i] || $str[$i] === "0")
+	{
+		if (!strcmp($str[$i], $key))
+		{
+			$val = $str[$i + 1] . "\n";
+			$hel = true;
+		}
+		$i = $i + 2;
+	}
+	if ($hel)
+		echo $val;
 }
 else {
 	return ;
